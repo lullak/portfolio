a9000af3c1e8aa82959ce99587f46e9f5246343e
Dennis <dennisbircan@gmail.com>
2024-12-03 15:22:52 +0100

    Imported bootstrap dependency for hamburgermenu

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "../index.css";
 import logo from '../assets/images/db_logo.png';
