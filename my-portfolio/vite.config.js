26dbec27271607cedcd551a02c732f6a0142c4e1
Dennis <dennisbircan@gmail.com>
2025-01-09 14:05:15 +0100

    small changes

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'esbuild', // Use esbuild for minification
+    minify: 'esbuild', 
   },
 })
