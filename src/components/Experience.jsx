cc22f136d2ce3b2d56b78669dc4fcb650dc9c730
Dennis <dennisbircan@gmail.com>
2025-01-10 18:23:27 +0100

    text change

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,8 +12,10 @@ const Experience = () => (
         <h3>Operations and Process Manager – Investor Relations</h3>
         <p><em>2018-2024</em></p>
         <p>
-          Experienced Operations and Process Manager with a strong background in leadership, regulatory compliance, and investor relations.
-        </p>
+          Experienced Operations and Process Manager with a strong background in leadership, regulatory
+          compliance, and investor relations. Skilled in managing daily operations, improving processes, and
+          supporting CEOs and CFOs of publicly listed companies. Expertise in regulatory adherence (Nasdaq
+          Stockholm, First North, etc.), project management, and developing IR and corporate websites.</p>
         <a href={cv} target="_blank" className="btn btn-primary mb-3">
           <i className="fas fa-download"></i> Download my CV
         </a>
