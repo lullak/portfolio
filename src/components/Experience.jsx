279ca81ad46f68069ed5bf8cfd6e21c14b8b0191
Dennis <dennisbircan@gmail.com>
2025-01-10 18:28:57 +0100

    experience margins and boxsize

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ import cv from '../assets/files/resume.pdf';
 const Experience = () => (
   <section id="experience" className="container my-5">
     <div className="row">
-      <article className="col-md-12 col-12">
+      <article className="col-md-8 col-12 mx-auto">
         <h2>Prior Experience</h2>
         <h3>Operations and Process Manager – Investor Relations</h3>
         <p><em>2018-2024</em></p>
@@ -16,7 +16,7 @@ const Experience = () => (
           compliance, and investor relations. Skilled in managing daily operations, improving processes, and
           supporting CEOs and CFOs of publicly listed companies. Expertise in regulatory adherence (Nasdaq
           Stockholm, First North, etc.), project management, and developing IR and corporate websites.</p>
-        <a href={cv} target="_blank" className="btn btn-primary mb-3">
+        <a href={cv} target="_blank" className="btn btn-primary mb-3 mt-3">
           <i className="fas fa-download"></i> Download my CV
         </a>
       </article>
