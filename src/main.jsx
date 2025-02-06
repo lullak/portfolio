5e133d3895cbe4ae5e9ce9853158391b90d96317
Dennis <dennisbircan@gmail.com>
2025-02-06 20:21:59 +0100

    Google tag

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,31 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import favicon from './assets/images/db_logofav_white.png';
 
-const Main = () => {
-  useEffect(() => {
-    const link = document.createElement('link');
-    link.rel = 'icon';
-    link.href = favicon;
-    document.head.appendChild(link);
+const link = document.createElement('link');
+link.rel = 'icon';
+link.href = favicon;
+document.head.appendChild(link);
 
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-T57LY0S77Z';
-    document.head.appendChild(script);
+const script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-T57LY0S77Z';
+document.head.appendChild(script);
 
-    const script2 = document.createElement('script');
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-T57LY0S77Z');
-    `;
-    document.head.appendChild(script2);
-  }, []);
-
-  return <App />;
-};
+const script2 = document.createElement('script');
+script2.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-T57LY0S77Z');
+`;
+document.head.appendChild(script2);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Main />
+    <App />
   </StrictMode>,
 )
