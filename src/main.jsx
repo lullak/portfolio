259ff2849d8996184a468d58a75350995a4e08e3
Dennis <dennisbircan@gmail.com>
2025-02-06 20:09:40 +0100

    Testing google tag

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,33 @@ import './index.css'
 import App from './App.jsx'
 import favicon from './assets/images/db_logofav_white.png';
 
-const link = document.createElement('link');
-link.rel = 'icon';
-link.href = favicon;
-document.head.appendChild(link);
+const Main = () => {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = favicon;
+    document.head.appendChild(link);
+
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-T57LY0S77Z';
+    document.head.appendChild(script);
+
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-T57LY0S77Z');
+    `;
+    document.head.appendChild(script2);
+  }, []);
+
+  return <App />;
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Main />
   </StrictMode>,
 )
